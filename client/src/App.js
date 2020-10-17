641f1bae3ab18831843a1f91cd32dba81882bb67
“anitapeppercorn” <anita@peppercorn.ai>
2020-10-17 10:24:50 -0700

    improve success

--- client/src/App.js
+++ client/src/App.js
@@ -39,8 +39,8 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
               <Route exact path="/success" component={Success} />
+              <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
         </div>
